664197c0a46c3645631a67123e6ae40b6fb8e703
cerberux77 <gptmanuelvera@gmail.com>
2026-04-12 00:56:33 -0400

    chore: add prisma config, deps and generated client

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    // Le pasamos el DIRECT_URL porque este archivo lo usa el CLI para migrar
+    url: process.env["DIRECT_URL"],
   },
 });
