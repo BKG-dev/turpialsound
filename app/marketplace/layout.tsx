c3607cc3daf555df9d1e9accf9be704c8be10a6a
BKG-dev <jcarlosleon81@gmail.com>
2026-05-04 09:32:13 -0400

    fix(marketplace): wrap shell with session provider

--- app/marketplace/layout.tsx
+++ app/marketplace/layout.tsx
@@ -1,13 +1,16 @@
 import type { ReactNode } from 'react'
 import { MarketplaceThemeProvider } from '@/components/marketplace/MarketplaceTheme'
 import { MarketplaceAssistantFab } from '@/components/marketplace/MarketplaceAssistantFab'
+import { MarketplaceSessionProvider } from '@/components/marketplace/MarketplaceAuthBar'
 
 export default function MarketplaceLayout({ children }: { children: ReactNode }) {
   return (
     <MarketplaceThemeProvider className="mp-route-shell min-h-screen">
-      <MarketplaceAssistantFab>
-        {children}
-      </MarketplaceAssistantFab>
+      <MarketplaceSessionProvider>
+        <MarketplaceAssistantFab>
+          {children}
+        </MarketplaceAssistantFab>
+      </MarketplaceSessionProvider>
     </MarketplaceThemeProvider>
   )
 }
